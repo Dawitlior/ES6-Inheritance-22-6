f9fe5c4258df80dc65909164e202c4827bc3f47e
Dawitlior <dawitlior777@gmail.com>
2022-07-12 20:18:19 +0300

    add

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-// class Teacher {
-//   static counter = 0;
-//   constructor(fullName, age, salary) {
-//     this.fullName = fullName;
-//     this.age = age;
-//     this.salary = salary;
-//     Teacher.counter++;
-//   }
+class Teacher {
+  static counter = 0;
+  constructor(fullName, age, salary) {
+    this.fullName = fullName;
+    this.age = age;
+    this.salary = salary;
+    Teacher.counter++;
+  }
 
-//   static printOlderTeacher(firstTeacher, secundTeacher) {
-//     if (firstTeacher.age > secundTeacher.age) {
-//       console.log(firstTeacher);
-//       return;
-//     }
-//     console.log(secundTeacher);
-//   }
-// }
-// console.log(Teacher.counter);
-// const teacher1 = new Teacher("lior dawit", 25, 2000);
-// const teacher2 = new Teacher("yaso mlako", 26, 2300);
-// console.log(Teacher.counter);
-// printOlderTeacher(teacher1, teacher2);
-//------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------
+  static printOlderTeacher(firstTeacher, secundTeacher) {
+    if (firstTeacher.age > secundTeacher.age) {
+      console.log(firstTeacher);
+      return;
+    }
+    console.log(secundTeacher);
+  }
+}
+console.log(Teacher.counter);
+const teacher1 = new Teacher("lior dawit", 25, 2000);
+const teacher2 = new Teacher("yaso mlako", 26, 2300);
+console.log(Teacher.counter);
+printOlderTeacher(teacher1, teacher2);
+// ------------------------------------------------------------------------------------------------
 
 let someItem1 = 0;
 let someItem2 = 0;
